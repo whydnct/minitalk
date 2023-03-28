ce2d731ca7a4dadcfd8fa2cb9cd5a0ca8b0351a4
al <whydnct@gmail.com>
2023-03-28 20:38:45 +0200

    .

--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 18:22:31 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/03/28 07:35:30 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/03/28 20:38:22 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <fcntl.h>
+
+sig_atomic_t	flag;
 
 void	char_printer(int bit, int client_pid)
 {
@@ -49,26 +51,36 @@ void	char_printer(int bit, int client_pid)
 void	action(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
-	char_printer(signal == SIGUSR1, info->si_pid);
+	flag = 0;
+	if (signal == SIGUSR1)
+		flag = 1;
+	char_printer(flag, info->si_pid);
 }
 
-int	main(void)
+void	set_signal_action(void)
 {
 	struct sigaction	sa;
-	pid_t				pid;
-	int					fid;
 
-	fid = open("./_id", O_WRONLY | O_CREAT, 0644);
-	pid = getpid();
-	ft_putnbr_fd(pid, 1);
-	ft_putnbr_fd(pid, fid);
-	close(fid);
-	write(1, "\n", 1);
+	ft_bzero(&sa, sizeof(sa));
 	sa.sa_sigaction = &action;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+}
+
+int	main(void)
+{
+	pid_t				pid;
+	int					fid;
+
+	pid = getpid();
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	fid = open("./_id", O_WRONLY | O_CREAT, 0644);
+	ft_putnbr_fd(pid, fid);
+	close(fid);
+	set_signal_action;
 	while (1)
 	{
 		pause();
