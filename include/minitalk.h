b8cf26b7111fcbf23a0b92e1e7b79d251d2a2f5e
whydnct <82403599+whydnct@users.noreply.github.com>
2023-04-14 10:56:45 +0000

    .

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 20:01:04 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/04/13 18:25:53 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/04/14 10:56:16 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # define _POSIX_C_SOURCE 200112L
 
 # include <signal.h>
-# include <stdlib.h>
 # include <unistd.h>
 
 # include "ft.h"
