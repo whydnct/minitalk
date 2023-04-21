84709fdd29944ea0382c6bf2fc1b9230f2a4bb12
al <whydnct@gmail.com>
2023-04-21 08:02:10 +0200

    add safe_stop

--- src/server_bonus.c
+++ src/server_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 18:22:31 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/04/15 07:39:26 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/04/21 08:01:20 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@ void	char_printer(int bit, int client_pid)
 {
 	static int				i = 0;
 	static unsigned char	c = 0;
+	int						safe_stop = 0;
 
 	c |= (bit << i);
 	i++;
-	while (kill(client_pid, SIGUSR1) == -1)
+	while (kill(client_pid, SIGUSR1) == -1 && ++safe_stop < 10)
 		write(2, "error sending SIGUSR1 to client\n", 33);
+	if (safe_stop == 10)
+		exit(1);
 	if (i > 7)
 	{
 		if (c)
 			write(1, &c, 1);
 		else
 		{
-			while (kill(client_pid, SIGUSR2) == -1)
+			while (kill(client_pid, SIGUSR2) == -1 && ++safe_stop < 20)
 				write(2, "error sending SIGUSR2 to client\n", 33);
+			if (safe_stop == 20)
+				exit(1);
 		}
 		c = 0;
 		i = 0;
