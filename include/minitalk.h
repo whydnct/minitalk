277a7b27670ba2f3f117225ca7132cbcc4acf6b3
aperez-m <aperez-m@student.42urduliz.com>
2023-04-23 20:01:00 +0200

    mend

--- include/minitalk.h
+++ include/minitalk.h
@@ -3,18 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: aperez-m <aperez-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 20:01:04 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/04/23 12:06:33 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/04/23 20:00:47 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-
-//# define _XOPEN_SOURCE 500
-//# define _POSIX_C_SOURCE 200112L
 
 # include <signal.h>
 # include <unistd.h>
